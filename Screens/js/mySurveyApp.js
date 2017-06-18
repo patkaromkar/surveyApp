61685d03ef76b4a22a2c03502d8de6362bfbfd91
Omkar Patkar <patkaromkar@gmail.com>
2017-06-18 15:34:19 +0530

    Added Validation on login button click: - 1) Username field cannot be empty 2) Password field cannot be empty 3) If both fields are not empty then the username and the password must be correct.

--- Screens/js/mySurveyApp.js
+++ Screens/js/mySurveyApp.js
@@ -3,21 +3,53 @@ var app = angular.module("myApp",[]);
 	app.controller("myCntrl1", ctrl1);
 	
 	function ctrl1() {
-		//this.uName = "Omkar";
+		console.log("Start of ctrl1");
 		this.submitLogin = login;
+		this.uName = "";
+		this.passWd = "";
+		
 		this.isCredsInvalid = false;
+		console.log("End of ctrl1");
+	}
+	
+	function login(mySALoginForm) {
+		console.log("Start of login >"+this.uName+"<");
+		this.isCredsInvalid = false;
+		var isUnameEmpty = checkIfEmtpy(mySALoginForm.uname.$viewValue);
+		if (isUnameEmpty) {
+			mySALoginForm.uname.$invalid = true;
+		} 
+		
+		var isPwdEmpty = checkIfEmtpy(mySALoginForm.pwd.$viewValue);
+		if (isPwdEmpty) {
+			mySALoginForm.pwd.$invalid = true;
+		}
+		
+		if (mySALoginForm.uname.$invalid || mySALoginForm.pwd.$invalid) {
+			mySALoginForm.$invalid = true;
+		} 
+		
+		if (!mySALoginForm.$invalid) {
+			if (this.uName == "omkar" && this.passWd == "asd") {
+				console.log("Creds are correct");
+				this.isCredsInvalid = false;
+				
+			} else {
+				this.isCredsInvalid = true;
+				
+			}
+		}
+		
+		console.log("End of login() this.isCredsInvalid => "+this.isCredsInvalid);
+		return this.isCredsInvalid;
 		
 	}
 	
-	function login() {
-		console.log(this.uName);
-		console.log(this.passWd);
-		if (this.uName == "omkar" && this.passWd == "asd") {
-			console.log("Creds are correct");
-			this.isCredsInvalid = false;
+	function checkIfEmtpy(myVal) {
+		console.log("Inside checkIfEmpty()");
+		if (myVal == "") {
 			return true;
 		} else {
-			this.isCredsInvalid = true;
 			return false;
 		}
 		
